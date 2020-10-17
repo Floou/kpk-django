c7660170717472990d2cf97ffbb8446009753fd9
Slick <holygalaxy444@gmail.com>
2020-10-17 14:29:54 +0500

    mainapp

--- kpkdjango/kpkdjango/settings.py
+++ kpkdjango/kpkdjango/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'j6b_=k4(1+4@ue@$a^_-f=jw8&kz=f#gpqam_k!0%ep2n#k_@q'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'mainapp',
 ]
 
 MIDDLEWARE = [

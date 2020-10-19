2eb6c187e6f795f858ad6b052dbd92ce9b966554
Slick <holygalaxy444@gmail.com>
2020-10-19 11:06:01 +0500

    201019

--- kpkdjango/mainapp/admin.py
+++ kpkdjango/mainapp/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
-# Register your models here.
+from mainapp.models import SubjectCategory, Course
+
+admin.site.register(SubjectCategory)
+admin.site.register(Course)
--- kpkdjango/mainapp/models.py
+++ kpkdjango/mainapp/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class SubjectCategory(models.Model):
+    name = models.CharField(max_length=128)
+    desc = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Course(models.Model):
+    category = models.ForeignKey(SubjectCategory,
+                                 on_delete=models.CASCADE)
+    name = models.CharField(max_length=200)
+    desc = models.TextField(blank=True)
+    hours = models.IntegerField(default=0)
+    is_active = models.BooleanField(default=0)
